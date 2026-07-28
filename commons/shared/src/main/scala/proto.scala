f214280a470b7abde5ce6f06870fd85eacf0b73f
Leandro Cruz <leandro.saad@gmail.com>
2026-07-28 17:14:54 -0300

    Fixed json code for proto types

--- commons/shared/src/main/scala/proto.scala
+++ commons/shared/src/main/scala/proto.scala
@@ -9,11 +9,13 @@ object proto {
   case class VerifyMorbidTokenRequest(token: String)
   case class SetClaimsRequest(uid: String, claims: Map[String, String])
   case class GetLoginMode(email: Email, tenant: Option[TenantCode])
-  case class EmitToken(email: Email, magic: Magic, days: Option[Int]) derives JsonCodec
-  case class SwapTokenRequest(token: String, magic: Magic) derives JsonCodec
+  case class EmitToken(email: Email, magic: Magic, days: Option[Int])
+  case class SwapTokenRequest(token: String, magic: Magic)
 
-  given JsonDecoder[VerifyGoogleTokenRequest] = DeriveJsonDecoder.gen
-  given JsonDecoder[VerifyMorbidTokenRequest] = DeriveJsonDecoder.gen
-  given JsonDecoder[SetClaimsRequest]         = DeriveJsonDecoder.gen
-  given JsonDecoder[GetLoginMode]             = DeriveJsonDecoder.gen
+  given JsonCodec[VerifyGoogleTokenRequest] = DeriveJsonCodec.gen
+  given JsonCodec[VerifyMorbidTokenRequest] = DeriveJsonCodec.gen
+  given JsonCodec[SetClaimsRequest]         = DeriveJsonCodec.gen
+  given JsonCodec[GetLoginMode]             = DeriveJsonCodec.gen
+  given JsonCodec[EmitToken]                = DeriveJsonCodec.gen
+  given JsonCodec[SwapTokenRequest]         = DeriveJsonCodec.gen
 }
